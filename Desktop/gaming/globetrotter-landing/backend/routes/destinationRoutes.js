d468b829f71a7b1386a46ead5518598879b6e992
Ritik Jaykar <113467380+Ritikjaykar@users.noreply.github.com>
2025-04-17 02:03:19 +0530

    Delete Desktop directory

--- Desktop/gaming/globetrotter-landing/backend/routes/destinationRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getRandomDestination } from '../controllers/destinationController.js';
-
-const router = express.Router();
-
-router.get('/random', getRandomDestination);
-
-export default router;
